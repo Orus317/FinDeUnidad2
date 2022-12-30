f8cb80749b9e6818fac252a4fe41e7b941345499
Oriol Palacios <orioloriolpalacios@outlook.com>
2022-12-30 17:18:35 -0500

    Implementación  de CObjeto

---
+++ ClasesGenerales/CObjeto.cs
@@ -0,0 +1,60 @@
+﻿namespace ClasesGenerales
+{
+    // Acá van las clases: CObjeto, CListaObjetos (que debe funcionar con la lista recursiva)
+    public class CObjeto
+    {
+        #region  ATRIBUTOS 
+        private string _Id;
+
+        public string Id { get => _Id; set => _Id = value; }
+        #endregion
+
+        #region  CONSTRUCTORES
+        public CObjeto()
+        {
+            aId = "";
+        }
+        public CObjeto(string pId)
+        {
+            aId = pId;
+        }
+        #endregion   
+
+        #region MÉTODOS     
+        // ================================================================
+        public override string ToString()
+        {
+            return Id;
+        }
+
+        // ================================================================
+        public override bool Equals(object Objeto)
+        {
+            return (Objeto.ToString() == ToString());
+        }
+
+        // ================================================================
+        public virtual void Leer()
+        {
+            Console.Write("Identificador: ");
+            Id = Console.ReadLine();
+        }
+
+        // ================================================================
+        public virtual void Escribir()
+        {
+            Console.WriteLine(Id);
+        }
+
+        // ================================================================
+        public virtual void Mostrar()
+        {
+            Console.WriteLine();
+            Console.WriteLine("DATOS DE OBJETO");
+            Console.WriteLine("================");
+            Console.WriteLine("Identificador: " + Id);
+        }
+        #endregion          
+
+    }
+}
--- ClasesGenerales/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ClasesGenerales
-{
-    // Acá van las clases: CObjeto, CListaObjetos (que debe funcionar con la lista recursiva)
-    public class Class1
-    {
-
-    }
-}
