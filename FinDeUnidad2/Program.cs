ba8d61adffcd2ccc3954b76128004e9042fd6164
Oriol Palacios <orioloriolpalacios@outlook.com>
2023-01-03 15:58:36 -0500

    Me olvidé de comentar en el trasanterior commit que la clase CLector, CControlLectores y CListaLectores fueron completadas y habilitadas para su uso

--- FinDeUnidad2/Program.cs
+++ FinDeUnidad2/Program.cs
@@ -4,15 +4,6 @@
 using ClasesGenerales;
 using EstDatos;
 
-CListaObjetos a = new();
-CObjeto uno = new("1");
-CObjeto dos = new("2");
-CObjeto tres = new("3");
-a.Agregar(uno);
-a.Agregar(dos);
-a.Agregar(tres);
-a.Listar();
-
 
 
 
