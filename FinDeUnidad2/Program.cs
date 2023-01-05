e28add6fc6fc0853c33cc7331bafc27f6b56fd21
Ian Quispe <iancillonn@gmail.com>
2023-01-04 20:03:33 -0500

    Implementando el menú en Program.cs

--- FinDeUnidad2/Program.cs
+++ FinDeUnidad2/Program.cs
@@ -1,10 +1,19 @@
-﻿// Programa principal
+﻿using System;
+using AppBiblioteca;
+// Programa principal
 // Pruebas de funcionamiento para lista recursiva (se tiene que borrar)
 
 using ClasesGenerales;
 using EstDatos;
 
+CControlTesis Tesis = new();
+CControlLectores Lectores = new();
+CControlPrestamo Prestamos = new();
 
+while (true)
+{
+	CMenu.MostrarMenu(Tesis, Lectores, Prestamos);
+}
 
 
 // ----------------------------------------------------------------------
